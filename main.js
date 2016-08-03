6847a11cb2685aa837db9e14c8a49f1bd26faee6
Ikuo Degawa <degawa@ikuwow.com>
2016-08-03 20:43:23 +0900

    Add basic scroll functions

--- main.js
+++ main.js
@@ -1,3 +1,38 @@
+(function(){
+
 "use strict";
 
-console.log('hello');
+let keyCodes = {
+    A: 65,
+    E: 69,
+    N: 78,
+    P: 80
+};
+
+let minScrollLength = 36;
+
+document.addEventListener('keydown', function(e) {
+
+    var scrollDestination;
+
+    if (e.ctrlKey) {
+        if (e.keyCode == keyCodes.A) {
+            scrollDestination = window.pageYOffset - window.innerHeight;
+            window.scroll(0, scrollDestination);
+        }
+        if (e.keyCode == keyCodes.E) {
+            scrollDestination = window.pageYOffset + window.innerHeight;
+            window.scroll(0, scrollDestination);
+        }
+        if (e.keyCode == keyCodes.N) {
+            scrollDestination = window.pageYOffset + minScrollLength;
+            window.scroll(0, scrollDestination);
+        }
+        if (e.keyCode == keyCodes.P) {
+            scrollDestination = window.pageYOffset - minScrollLength;
+            window.scroll(0, scrollDestination);
+        }
+    }
+});
+
+})();
